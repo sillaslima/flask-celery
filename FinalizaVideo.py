5d98566742f82425e656f988dc8ca172dcf5e186
sillaslima <sillas.lima@globo.com>
2017-11-04 02:00:58 +0000

    ajustes no FinalizaVideo

--- FinalizaVideo.py
+++ FinalizaVideo.py
@@ -4,6 +4,7 @@ import glob
 import subprocess
 from random import randint
 import threading
+import pdb
 
 
 def geraNome():
@@ -21,6 +22,11 @@ def retira_audio(video):
 	saida_audio=an.wait()
 	if saida_audio == 0:
 		print ('AUDIO RETIRADO COM SUCESSO',nome_audio)
+		#rm = os.unlink(sem_audio)
+		#rm = os.unlink(video_slow)
+		mv = shutil.move(sem_audio, './Audio')
+		print('move com sucesso')
+
 	else:
 		print ('PROBLEMAS AO RETIRAR O AUDIO')
 	return nome_audio
@@ -42,57 +48,25 @@ def gerar_slow(video_gerar_slow):
 	saida_slow = slow_motion.wait()
 	if saida_slow == 0:
 		print('SLOW MOTION GERADO COM SUCESSO',nome_slow)
+		#mv = shutil.move(nome_slow, './Slow')
+		#rm = os.unlink(slow)
+		#print('move SLOW com sucesso')
+		#print('remove SLOW com sucesso')
+
 		
 	else:
 		print('PROBLEMAS COM SLOW MOTION')
 	return nome_slow
-def concatena_slow (video_audio, video_slow):
-	print("--------VAMOS CONCATENAR O VIDEO SEM AUDIO COM SLOW MOTION--------")
-	print("--------VIDEO SEM AUDIO RECEBIDO",video_audio)
-	print("--------VIDEO ORIGINAL RECEBIDO",video_slow)
-	nome_concatena=geraNome()
-	concatenar = [
-	"ffmpeg",
-	 "-i",
-	 video_audio,
-	 "-i",
-	 video_slow,
-	 "-filter_complex",
-	 "[0:v] [1:v] concat=n=2:v=1 [v]",
-	 "-map","[v]",
-	 nome_concatena]
-	print(concatenar)
-	print('----------------APLICANDO PARA CONCATENAR VIDEO SEM AUDO COM SLOW MOTION------------------')
-	concat_video = subprocess.Popen(concatenar,stdout=subprocess.PIPE, stderr=subprocess.STDOUT,universal_newlines=True)
-	saida_concat = concat_video.wait()
-	if saida_concat == 0:
-		print('VIDEOS CONCATENADO COM SUCESSO',nome_concatena)
-		
-	else:
-		print('PROBLEMAS AO CONCATENAR VIDEOS')
-	return nome_concatena
 
 def concatena_slow (video_audio, video_slow):
 	print("--------VAMOS CONCATENAR O VIDEO SEM AUDIO COM SLOW MOTION--------")
 	print("--------VIDEO SEM AUDIO RECEBIDO",video_audio)
 	print("--------VIDEO ORIGINAL RECEBIDO",video_slow)
-	video_intro="/home/sillas/futTerca/Futshowme/vide_extraido/nome_concatena.avi"
+	video_intro="/home/pi/app/flask-celery/static/videos/fechamento.avi"
 	nome_concatena=geraNome()
 	print(video_intro)
 	concatenar = [
-	# "ffmpeg",
-	#  "-i",
-	#  video_audio,
-	#  "-i",
-	#  video_slow,
-	#  "-filter_complex",
-	#  "[0:v] [1:v] concat=n=2:v=1 [v]",
-	#  "-map","[v]",
-	#  nome_concatena
-	#"ffmpeg",
-	#"-i", video_intro, "-i", video_intermediario,
-	 #"-filter_complex", "[0:v] [1:v] concat=n=2:v=1 [v]", "-map","[v]", nome_final]
-
+	
 	"ffmpeg",
 	 "-i",
 	 video_audio,
@@ -113,23 +87,22 @@ def concatena_slow (video_audio, video_slow):
 		print('VIDEOS CONCATENADO COM SUCESSO',nome_concatena)
 		rm = os.unlink(video_audio)
 		rm = os.unlink(video_slow)
-		mv = shutil.move(nome_concatena, './tmp/video_reproduzir/')
+		
+		mv = shutil.move(nome_concatena, './Reproduzir')
 		print('move com sucesso')
 	else:
 		print('PROBLEMAS AO CONCATENAR VIDEOS')
 	return nome_concatena
 
-
-print ('---------INICIANDO A VERIFICAÇÃO E EDIÇÃO DE VIDEOS---------')
-roda = True
-
-while True:
-	verifica_video = int(input('1- para verificar se existe novo arquivo ou 2- para Cancelar a verificação: '))
+def time_line_finaliza_video(verifica_video):
+	print ('---------INICIANDO A VERIFICAÇÃO E EDIÇÃO DE VIDEOS---------')
+	#pdb.set_trace()
+	verifica_video = verifica_video
 	
 	if verifica_video == 1:
 		print ('----------------Verificando se existem VIDEOS na pasta-----------------')
 		lista_videos_retirar_audio = glob.glob('./tmp/video_extraido/*.avi')
-		print(type(lista_videos_retirar_audio))
+		print(lista_videos_retirar_audio)
 
 		if len(lista_videos_retirar_audio) > 0:
 			print('Lista de videos na pasta',lista_videos_retirar_audio)
@@ -139,15 +112,21 @@ while True:
 					
 				nome_audio=retira_audio(lista_videos_retirar_audio[i])
 				print('Video sem Audio Gerado',nome_audio)
-				#print('tempo de execução', (end_time_retira_audio - start_retira_audio))
+				
 				print('chamando a função para gerar o SLOW',nome_audio)
+
 				gera_slow=gerar_slow(nome_audio)
+				
 				print('Video com Slow Motion gerado',gera_slow)
+				
 				print('chamando a função para concatenar videos',gera_slow)
-				print('e',nome_audio)
+				
+						
 				#gera_concatena=concatena_slow(nome_audio,gera_slow)
-				th = threading.Thread(target=concatena_slow, args=(nome_audio,gera_slow))
-				th.start()
+				concatena_slow(nome_audio,gera_slow)
+				#th = threading.Thread(target=concatena_slow, args=(nome_audio,gera_slow))
+				
+				#th.start()
 				#print('Video Concatenado gerado com sucesso')
 	#print('Video Concatenado gerado com sucesso')
 			# for i in range(len(lista_videos_retirar_audio)):
@@ -171,5 +150,6 @@ while True:
 		if verifica_video == 2:
 			roda = False
 			print('saindo')
-			break
+			#break
 			exit(1)
+	return 'time line de video gerada'
--- teste.py
+++ teste.py
@@ -1,6 +1,7 @@
 from cronometro import gera_momento, gravar
 from VerificaWorkspace import verifica_ws
-verifica_ws()
+from FinalizaVideo import time_line_finaliza_video
+time_line_finaliza_video(1)
 #gravar()
 #gera_momento()
 
